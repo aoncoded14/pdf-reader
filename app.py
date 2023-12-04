58ae7b7b0b3ec47cd5778c153ad49f787ae5537e
aoncoded14 <dorkuma958@gmail.com>
2023-12-04 19:03:35 +0100

    hugging face test with streamlit

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+from dotenv import load_dotenv
+
+from PyPDF2 import PdfReader
+
+from langchain.text_splitter import CharacterTextSplitter
+
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+from langchain.vectorstores import  FAISS
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl"),
+    vectorstore = FAISS.from_texts(texts=text_chunks,embeddings=embeddings)
+    return vectorstore
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="LangChain Test", page_icon=":car:")
+
+    st.header("Chat with multiple PDFs :books:")
+    st.text_input("Ast questions about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your pdfs here and click 'Process' ", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+            #get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                
+            #get text chuncks
+                text_chunks = get_text_chunks(raw_text)
+                
+            #create vector store
+                vectorstore = get_vectorstore(text_chunks)
+
+
+
+if __name__ == ('__main__'):
+    main( )
